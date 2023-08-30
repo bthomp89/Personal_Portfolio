052ac90b2370c06abb72af18cde25ca1733a1eeb
Brayden Thompson <bthomp89@uwo.ca>
2023-08-30 13:35:15 -0400

    moved experience header to side to mix it up

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 export default function Experience({}: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-left max-w-full px-10 justify-evenly mx-auto items-center mt-32 mb-60">
+    <div className="flex flex-col relative h-screen text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center mt-32 mb-60">
       <h3 className="relative -top-24 uppercase tracking-[20px] text-gray-500 text-2xl pt-52 pb-24">
         Experience
       </h3>
